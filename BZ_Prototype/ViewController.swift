aa5d65e4f778525f76725a53b4e1566e7a58d92f
Cagan <cagan123@gmail.com>
2017-02-26 17:10:33 -0600

    Initial Commit

---
+++ BZ_Prototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BZ_Prototype
+//
+//  Created by caganhawthorne on 2/26/17.
+//  Copyright © 2017 Cagan Hawthorne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
